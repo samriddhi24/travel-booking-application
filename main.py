e03c1fda30dc5d69294957ab64c1b32ba4368074
Samriddhi Chaudhary <sam.chaudhary2404@gmail.com>
2023-08-31 10:14:24 +0530

    hello

--- main.py
+++ main.py
@@ -8,24 +8,6 @@ class TravelBookingApp:
         self.root = root
         self.root.title("Travel Booking App")
 
-        '''self.destination_var = tk.StringVar(value=list(self.destinations.keys()))
-        self.destination_label = tk.Label(root, text="Select Destination:")
-        self.destination_label.pack()
-
-        self.destination_menu = tk.OptionMenu(root, self.destination_var, *self.destinations.keys())
-        self.destination_menu.pack()
-
-        self.book_button = tk.Button(root, text="Book Now", command=self.book_destination)
-        self.book_button.pack()
-
-    def book_destination(self):
-        selected_destination = self.destination_var.get()
-        if selected_destination in self.destinations:
-            price = self.destinations[selected_destination]["price"]
-            messagebox.showinfo("Booking Confirmation", f"You've booked {selected_destination} for ${price}. Enjoy your trip!")
-        else:
-            messagebox.showwarning("Booking Error", "Selected destination not available.")
-'''
 if __name__ == "__main__":
     root = tk.Tk()
     root["bg"]="white"
